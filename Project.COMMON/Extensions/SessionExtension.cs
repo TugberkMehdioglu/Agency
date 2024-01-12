79899d41079b022fe68eee0f4cbccb7ab959fdb5
Tuğberk <tugberkmehdioglu@gmail.com>
2024-01-12 14:22:42 +0300

    tier added

---
+++ Project.COMMON/Extensions/SessionExtension.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Project.COMMON.Extensions
+{
+    public static class SessionExtension
+    {
+        public static void SetSession<T>(this ISession session, string key, T value) where T : class
+        {
+            string serializedValue = JsonSerializer.Serialize(value);
+            session.SetString(key, serializedValue);
+        }
+
+        public static T? GetSession<T>(this ISession session, string key) where T : class
+        {
+            string? serializedValue = session.GetString(key);
+            if (string.IsNullOrEmpty(serializedValue)) return null;
+
+            try
+            {
+                return JsonSerializer.Deserialize<T>(serializedValue);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
